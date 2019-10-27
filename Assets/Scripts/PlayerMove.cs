bda41d38cec1a9dbea7d7b3a8cbcfecde4819844
Chris <cddmello@gmail.com>
2019-10-26 20:24:14 -0500

    added

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+using UnityEngine.Video;
+public class PlayerMove : MonoBehaviour
+{
+    public float speed = 2f;
+    public bool canMove = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+
+    void Update()
+    {
+        // Move forward when Default GearVR button pressed
+        if (Input.GetButton("Fire1"))
+        {
+            Vector3 cameraForward = Camera.main.transform.forward;
+            this.gameObject.transform.Translate(cameraForward * Time.deltaTime * speed);
+            Debug.Log("testing");
+        }
+    }
+
+}
